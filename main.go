f01e518f4d27b2d2d526a53bf10719cc2a157b97
ba.thai <ba.thai@tiki.vn>
2021-02-09 13:32:09 +0700

    add more code

--- main.go
+++ main.go
@@ -4,9 +4,56 @@ import (
 	"fmt"
 	"github.com/hashicorp/yamux"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 	"net"
 )
+
+type Config struct {
+	Services []ServiceDef // list service definitions
+}
+
+type ServiceDef struct {
+	Address string // TCP address
+	Name    string // Name of service
+}
+
+func runDaemon(address string) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		go func() {
+			fmt.Println("accept new conn from", conn.RemoteAddr().String())
+			session, err := yamux.Server(conn, nil)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			go func() {
+				for {
+					stream, err := session.Accept()
+					if err != nil {
+						fmt.Println(err.Error())
+						return
+					}
+
+					// we can exchange data from here
+					_ = stream
+					_, err = stream.Write([]byte("hello, i'm tunneld"))
+					if err != nil {
+						fmt.Println(err.Error())
+					}
+				}
+			}()
+		}()
+	}
+}
 
 func main() {
 	rootCmd := cobra.Command{
@@ -22,6 +69,8 @@ func main() {
 		Short: "daemon",
 		Long:  "daemon",
 		Run: func(cmd *cobra.Command, args []string) {
+			runDaemon("localhost:9999")
+
 			fmt.Println("daemon")
 			listener, err := net.Listen("tcp", "localhost:9999")
 			conn, err := listener.Accept()
@@ -60,9 +109,9 @@ func main() {
 			}
 			stream, err := session.Open()
 			fmt.Println("openned a stream")
-			//buf := make([]byte, 100)
-			data, _ := ioutil.ReadAll(stream)
-			fmt.Println("from server:", string(data))
+			buf := make([]byte, 512)
+			stream.Read(buf)
+			fmt.Println("from server:", string(buf))
 		},
 	}
 
---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+type echo struct {
+	Name string
+}
+
+// echoService send whatever it receives
+type echoMessage struct {
+	Msg string
+}
+
+// Start starts a tcp server on random port and accepts only one client
+// The tcp server responses any messages received from client
+// It returns the port to which the server is binding
+func (e *echo) Start() int {
+	listener, err := net.Listen("tcp", ":0")
+	checkErr(err)
+	go func() {
+		conn, err := listener.Accept()
+		checkErr(err)
+		_, err = io.Copy(conn, conn)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+
+			fmt.Println("echo: error while reading or writing, ", err.Error())
+		}
+	}()
+	return listener.Addr().(*net.TCPAddr).Port
+}
+
+// checkErr panics if err is not null
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
